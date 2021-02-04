683679ee954560897737a368ff7c4c76abcd73e9
Nick <nicky.kravtsov@gmail.com>
2021-02-04 15:02:36 +0200

    add horizontal scroll and fix media file

--- app/js/site_private.js
+++ app/js/site_private.js
@@ -5156,3 +5156,14 @@ $(document).ready(function() {
             }, 500)
         })
 });
+
+$(document).ready(function() {
+
+    $('html, body, *').mousewheel(function(e, delta) {
+        // multiplying by 40 is the sensitivity, 
+        // increase to scroll faster.
+        this.scrollLeft -= (delta * 40);
+        e.preventDefault();
+    });
+
+});
